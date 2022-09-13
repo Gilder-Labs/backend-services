ec5370e59c8c4c6a382259539410b0bb2a7af2e3
Ryan Szemplinski <ryan.szemplinski@pieinsurance.com>
2022-09-12 20:39:29 -0600

    Setting up nestjs using vite as the build tool for notify service

---
+++ services/notify-service/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+if (import.meta.env.PROD) {
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+  }
+  bootstrap();
+}
+
+export const viteNodeApp = NestFactory.create(AppModule);
