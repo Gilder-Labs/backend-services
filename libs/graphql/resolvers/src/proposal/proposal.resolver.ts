d303b7b7981fc1539ca894da77020bd056480f7d
Ryan Szemplinski <ryan.szemplinski@pieinsurance.com>
2022-10-04 12:05:52 -0600

    Adding event emitter

--- libs/graphql/resolvers/src/proposal/proposal.resolver.ts
+++ libs/graphql/resolvers/src/proposal/proposal.resolver.ts
@@ -5,31 +5,10 @@ import { Args, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver('Proposal')
 export class ProposalsResolver {
-  constructor(
-    private readonly proposalService: ProposalsService,
-    private readonly proposalRpcService: ProposalRPCService,
-  ) {}
+  constructor(private readonly proposalService: ProposalsService) {}
 
   @Query(() => [Proposal])
   async proposals(): Promise<Proposal[]> {
     return this.proposalService.getAllProposals();
-  }
-
-  @Query(() => [Proposal])
-  async getProposalsByRealm(
-    @Args('realmPk') realmPk: string,
-    @Args('programPk') programPk: string,
-  ): Promise<Proposal[]> {
-    const realm = {
-      realmPk,
-      programPk,
-    } as Realm;
-    const solanaPropsosals =
-      await this.proposalRpcService.getProposalsFromSolanaByRealm(realm);
-
-    return await this.proposalRpcService.convertSolanaProposalToEntity(
-      realm,
-      solanaPropsosals,
-    );
   }
 }
