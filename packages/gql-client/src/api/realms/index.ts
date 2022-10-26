37d351842266a401828deb3981df8e2fb9761bd3
Ryan Szemplinski <ryan.szemplinski@pieinsurance.com>
2022-10-26 14:16:19 -0600

    Fixing types

--- packages/gql-client/src/api/realms/index.ts
+++ packages/gql-client/src/api/realms/index.ts
@@ -1,4 +1,5 @@
 import type { Realm } from '@gilder/types';
+import { PublicKey } from '@solana/web3.js';
 import { ApiClient } from '../../client';
 import {
   GET_ALL_REALMS,
@@ -11,32 +12,27 @@ import { getResults } from '../utils';
 import { GetRealmArgs, RealmWithProposals } from './types';
 import { transformRealm } from './utils';
 
-const getAllRealms = async (client: ApiClient): Promise<Realm[]> => {
+const getAllRealms = async (client: ApiClient) => {
   return getResults<Realm<string>[]>({ query: GET_ALL_REALMS }, client).then(
-    (data) => data.map<Realm>(transformRealm),
+    (data) => data.map(transformRealm),
   );
 };
 
-const getAllRealmsWithProposals = async (
-  client: ApiClient,
-): Promise<RealmWithProposals[]> => {
+const getAllRealmsWithProposals = async (client: ApiClient) => {
   return getResults<RealmWithProposals<string>[]>(
     {
       query: GET_ALL_REALMS_WITH_PROPOSALS,
     },
     client,
   ).then((data) =>
-    data.map<RealmWithProposals>((x) => ({
+    data.map((x) => ({
       ...transformRealm(x),
       proposals: x.proposals.map(transformProposal),
     })),
   );
 };
 
-const getRealm = async (
-  variables: GetRealmArgs,
-  client: ApiClient,
-): Promise<Realm> => {
+const getRealm = async (variables: GetRealmArgs, client: ApiClient) => {
   return getResults<Realm<string>, GetRealmArgs>(
     { query: GET_REALM, variables },
     client,
@@ -46,7 +42,7 @@ const getRealm = async (
 const getRealmWithProposals = async (
   variables: GetRealmArgs,
   client: ApiClient,
-): Promise<RealmWithProposals> => {
+) => {
   return getResults<RealmWithProposals<string>, GetRealmArgs>(
     { query: GET_REALM_WITH_PROPOSALS, variables },
     client,
